a6be6292888859061a6f2f5c0d93a01c42b5d58f
Adasumizox <darkdan099@gmail.com>
2021-05-03 23:38:33 +0200

    FML

--- blockchain/app.py
+++ blockchain/app.py
@@ -1,7 +1,7 @@
 import uvicorn
 
 from Blockchain import Blockchain
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
@@ -17,6 +17,34 @@ if __name__ == '__main__':
     def index(request: Request):
         return templates.TemplateResponse("index.html", {"request": request})
 
+    @app.get('/homepage', response_class=HTMLResponse)
+    def home(request: Request):
+        # TODO: Home html with option of checking balance, making transaction and checking blockchain
+        # Should have logic checking if user is actually logged
+        return templates.TemplateResponse("home.html", {"request": request})
+
+    @app.get('/login')
+    def login(request: Request):
+        result = "Enter your credentials"
+        return templates.TemplateResponse("login.html", {"request": request, "result": result})
+
+    @app.post('/login')
+    def login(request: Request, login: str = Form(...), password: str = Form(...)):
+        # TODO: Change result to method checking credentials
+        result = None
+        return templates.TemplateResponse("login.html", {"request": request, "result": result})
+
+    @app.get('/register')
+    def register(request: Request):
+        result = "Enter your credentials"
+        return templates.TemplateResponse("register.html", {"request": request, "result": result})
+
+    @app.post('/register')
+    def register(request: Request):
+        # TODO: Change result to method creating account
+        result = None
+        return templates.TemplateResponse("register.html", {"request": request, "result": result})
+
     @app.get('/chain')
     def get_chain():
         chain_data = []
@@ -24,52 +52,15 @@ if __name__ == '__main__':
             chain_data.append(block.__dict__)
         return json.dumps({"length": len(chain_data), "chain": chain_data})
 
-    # @app.get('/balance')
-    # def get_balance():
-    #     return None
-    #
-    # @app.post('/transaction')
-    # def make_transaction():
-    #     return None
-    #
-    # @app.post('/login')
-    # def login():
-    #     message = ''
-    #     if request.method == 'POST':
-    #         username = request.form.get('username')
-    #         password = request.form.get('password')
-    #
-    #         # if in database and password correct
-    #         message = "Correct username and password"
-    #         redirect()
-    #         # else
-    #         message = "Wrong username or password"
-    #
-    # @app.post('/register')
-    # def register():
-    #     message = ''
-    #     if request.method == 'POST':
-    #         email = request.form.get('email')
-    #         username = request.form.get('username')
-    #         password = request.form.get('password')
-    #
-    #         # if username already exist
-    #         message = "Username is taken please choose other nickname"
-    #         return redirect(url_for('registerpage'))
-    #         # else
-    #         message = "succesfully created"
+    @app.get('/balance')
+    def get_balance():
+        # TODO: Get balance of account
+        return None
 
-#    @app.get('/loginpage')
-#    def loginpage():
-#        return
+    @app.post('/transaction')
+    def make_transaction():
+        # TODO: Make transaction
+        return None
 
-#    @app.get('/registerpage')
-#    def registerpage():
-#        return
-#
-#     @app.get('/homepage')
-#     def home():
-#         # Should have logic checking if user is actually logged
-#         return templates.get_template('home.html')
 
     uvicorn.run(app, host="127.0.0.1", port=8000)
